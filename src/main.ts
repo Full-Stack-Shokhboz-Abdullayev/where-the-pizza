8048fd94fd40f1dbf4c4eed44cbd6d8647aee744
Shokhboz <shokhboz11abdullayev@gmail.com>
2022-05-22 17:29:37 +0500

    fix: port use from env

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { EnvironmentVariables } from './shared/config/dto/env-variables.dto';
 import { SwaggerConfig } from './shared/config/swagger/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: '*'
   });
+
+  const configService: ConfigService<EnvironmentVariables> =
+    app.get(ConfigService);
 
   SwaggerConfig(app);
 
-  await app.listen(3000);
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
